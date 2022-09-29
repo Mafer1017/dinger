7937555beeb207167583816ee6473cde91e3f24b
Marcus <marcusferreira23@gmail.com>
2022-09-28 19:10:15 -0700

    DRY results and add breaks to displayed results.

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,21 +2,26 @@ import React from 'react'
  
 const Result = ({ swing , pitchNum }) => {
   let result;
+  let result2;
+  let result3;
   if(swing){
     if(pitchNum > swing){
-      result = "Pitcher number: " + pitchNum
-      + " Swing number: " + swing + " Difference: " + (pitchNum - swing);
+      result = "Pitcher number: " + pitchNum;
+      result2 = " Swing number: " + swing;
+      result3 = " Difference: " + (pitchNum - swing);
     }
     else if(pitchNum < swing){
-      result = "Pitcher number: " + pitchNum
-      + " Swing number: " + swing + " Difference: " + Math.abs(pitchNum - swing);
+      result = "Pitcher number: " + pitchNum;
+      result2 = " Swing number: " + swing;
+      result3 = " Difference: " + Math.abs(pitchNum - swing);
     }
     else{
-      result = pitchNum + "Pitcher number: " + pitchNum
-      + " Swing number: " + swing + " Difference: " + (pitchNum - swing);
+      result = "Pitcher number: " + pitchNum;
+      result2 = " Swing number: " + swing;
+      result3 = " Difference: " + (pitchNum - swing);
     }
   }
-  return <h3>{result}</h3>
+  return <h3>{result}<br />{result2}<br />{result3}</h3>
 }
- 
+
 export default Result
